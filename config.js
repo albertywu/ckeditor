1a9e82a95b38a11328f1ddd67e253543f5978c5f
Albert Wu <albertywu@gmail.com>
2015-05-18 17:46:31 -0700

    remove unused plugins; add coatue styles

--- config.js
+++ config.js
@@ -8,27 +8,20 @@ CKEDITOR.editorConfig = function( config ) {
 	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbar = null;
+
 	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
+		{ name: 'basic', groups: ['basicstyles', 'list', 'indent', 'undo', 'mode'] }
 	];
 
-	// Remove some buttons provided by the standard plugins, which are
-	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
+	// Remove some buttons from toolbar
+	config.removeButtons = 'Strike,Subscript,Superscript';
+
+	// Remove plugins
+	config.removePlugins = 'elementspath,iframe,iframedialog,justify,language,mathjax,newpage,pagebreak,placeholder,preview,save,scayt,showblocks,smiley,stylesheetparser,templates,widget,wsc,uicolor,adobeair,magicline,maximize,resize';
+
+	// Simplify the dialog windows
+	config.removeDialogTabs = 'image:advanced;link:advanced';
 
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
